7be8c83d63d643abd1148cbe16325d4afcb04a22
Sebas <psvitasebas@gmail.com>
2022-09-17 23:32:21 -0500

    nuevas mejoras

--- app/src/main/java/pe/edu/ulima/pm/pm_nota1_herrera_huarcaya_taco/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/pm_nota1_herrera_huarcaya_taco/MainActivity.kt
@@ -3,10 +3,12 @@ package pe.edu.ulima.pm.pm_nota1_herrera_huarcaya_taco
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import pe.edu.ulima.pm.pm_nota1_herrera_huarcaya_taco.databinding.ActivityMainBinding
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     enum class Turn{
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         "η","θ","ι","κ","λ","μ","ν","ξ","ο","π","ρ","σ","ς","τ",
         "θ","φ","χ","ψ","ω")
 
+    private var colour = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,7 +58,20 @@ class MainActivity : AppCompatActivity() {
         boardList.add(binding.z2)
         boardList.add(binding.z3)
     }
-
+    private fun closeGame() {
+        for(button in boardList){
+            button.setOnClickListener {
+                resetBoard()
+            }
+        }
+    }
+    private fun openGame(){
+        for(button in boardList){
+            button.setOnClickListener {
+                boardTapped(button)
+            }
+        }
+    }
     //Checa
     fun boardTapped(view : View){
         if(view !is Button)
@@ -64,14 +80,17 @@ class MainActivity : AppCompatActivity() {
         //gana 0
         if(checkForVictory(O)){
             result("O")
+            closeGame()
         }
         //gana X
         if(checkForVictory(X)){
             result("X")
+            closeGame()
         }
         //empate
-        if(fullBoard()){
+        if(fullBoard() && !checkForVictory(X) && !checkForVictory(O) ){
             result("Draw")
+            closeGame()
         }
     }
 
@@ -113,13 +132,14 @@ class MainActivity : AppCompatActivity() {
 
     fun getRandomColor(): Int {
         val rnd = Random()
-        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+        return  Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
     }
 
     private fun resetBoard() {
         for(button in boardList){
             button.text = ""
-            binding.layoutTicTacToe.setBackgroundColor(getRandomColor())
+            colour = getRandomColor()
+            binding.layoutTicTacToe.setBackgroundColor(colour)
         }
 
 
@@ -135,6 +155,7 @@ class MainActivity : AppCompatActivity() {
 
         currentTurn = firstTurn
         setTurnLabel()
+        openGame()
     }
 
     private fun fullBoard(): Boolean {
@@ -174,4 +195,35 @@ class MainActivity : AppCompatActivity() {
         var O = "O"
         var X = "X"
     }
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putCharSequence("x1", binding.x1.text )
+        outState.putCharSequence("x2", binding.x2.text )
+        outState.putCharSequence("x3", binding.x3.text )
+        outState.putCharSequence("y1", binding.y1.text )
+        outState.putCharSequence("y2", binding.y2.text )
+        outState.putCharSequence("y3", binding.y3.text )
+        outState.putCharSequence("z1", binding.z1.text )
+        outState.putCharSequence("z2", binding.z2.text )
+        outState.putCharSequence("z3", binding.z3.text )
+        outState.putCharSequence("turnoJugador", binding.turnoJugador.text)
+        outState.putInt("color", colour)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        binding.x1.text = savedInstanceState.getCharSequence("x1", "")
+        binding.x2.text = savedInstanceState.getCharSequence("x2", "")
+        binding.x3.text = savedInstanceState.getCharSequence("x3", "")
+        binding.y1.text = savedInstanceState.getCharSequence("y1", "")
+        binding.y2.text = savedInstanceState.getCharSequence("y2", "")
+        binding.y3.text = savedInstanceState.getCharSequence("y3", "")
+        binding.z1.text = savedInstanceState.getCharSequence("z1", "")
+        binding.z2.text = savedInstanceState.getCharSequence("z2", "")
+        binding.z3.text = savedInstanceState.getCharSequence("z3", "")
+        binding.turnoJugador.text = savedInstanceState.getCharSequence("turnoJugador", "")
+        colour = savedInstanceState.getInt("color",0)
+        binding.layoutTicTacToe
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
 }
